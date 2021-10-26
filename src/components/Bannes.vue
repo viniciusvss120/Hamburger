08fc9cb68b4dc5943c9b65ab8d32763798f111da
Vinicius Silva Vss120 <92170937+viniciusvss120@users.noreply.github.com>
2021-10-26 00:15:10 -0400

    Add files via upload

---
+++ src/components/Bannes.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="main-banner">
+        <h1>MAKE YOUR BURGER</h1>
+    </div>
+</template>
+
+<script>
+export default {
+ name: 'Banner',
+ data(){
+     return{
+
+     }
+ }
+
+
+}
+</script>
+
+<style scoped>
+     #main-banner {
+    background-image: url('/img/burger.jpg');
+    background-position: 0 -350px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  #main-banner h1 {
+    color: #FFF;
+    text-align: center;
+    font-size: 60px;
+    background-color: #222;
+    padding: 20px 40px;
+  }
+</style>

7ed4a35e0716f7737828e8f6297c176ea98b845c
Vinicius Silva Vss120 <92170937+viniciusvss120@users.noreply.github.com>
2021-10-26 00:13:28 -0400

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Dashboard from '../components/Dashboard.vue'
+export default {
+  name: 'About',
+  components: {
+    Dashboard
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <Bannes />
+    <h2>Monte seu burger:</h2>
+    <BurgerForm />
+  </div>
+</template>
+
+<script>
+import Bannes from '../components/Bannes.vue'
+import BurgerForm from '../components/BurgerForm.vue'
+export default {
+  name: 'Home',
+  components:{
+    Bannes,
+    BurgerForm
+  }
+ 
+}
+</script>
+
+<style scoped>
+  h2{
+    display: flex;
+    justify-content: center;
+    margin-bottom: 25px;
+    }
+</style>
